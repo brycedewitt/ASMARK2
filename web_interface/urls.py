2ffa08c95640df4643804b2964332ad4f9b0957c
bdewitt <dewitt.b@husky.neu.edu>
2019-06-01 15:56:17 -0400

    Updated URLs

--- web_interface/urls.py
+++ web_interface/urls.py
@@ -12,7 +12,7 @@ app_name = 'web_interface'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:drink_id>/', views.pour, name='pour'),
-    path('<int:beverage_id>/', views.pourShot, name='pourShot'),
+    path('/pourshot/<int:beverage_id>/', views.pourShot, name='pourShot'),
     path('shots/', views.ShotsView.as_view(), name='shots'),
     path('mix/', views.MixView.as_view(), name='mix'),
 ]
