4088c7d852749f231a5af8839a77364da1ea0655
bdewitt <dewitt.b@husky.neu.edu>
2019-08-03 17:55:30 -0400

    start and stop scripts (gpio) for testing physical pump connections

---
+++ stopall.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+gpio mode 17 out
+gpio mode 27 out
+gpio mode 22 out
+gpio mode 23 out
+gpio mode 24 out
+gpio mode 25 out
+gpio mode 20 out
+gpio mode 21 out
+
+gpio write 17 1
+gpio write 27 1
+gpio write 22 1
+gpio write 23 1
+gpio write 24 1
+gpio write 25 1
+gpio write 20 1
+gpio write 21 1
