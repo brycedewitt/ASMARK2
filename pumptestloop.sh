9796cc5570ce86994fda30ab9a71b625f9f8e45d
bdewitt <dewitt.b@husky.neu.edu>
2019-08-03 19:04:36 -0400

    changed to wiringPi pins

--- pumptestloop.sh
+++ pumptestloop.sh
@@ -6,8 +6,8 @@
 for i in 17 27 22 23 24 25 20 21
 do
   echo "working on pin $i"
-  gpio mode $i out
-  gpio write $i 0
+  gpio -g mode $i out
+  gpio -g write $i 0
   sleep 2s
-  gpio write $i 1
+  gpio -g write $i 1
 done
