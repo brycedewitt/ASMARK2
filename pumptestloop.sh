84540c3ae451123bdffc69194ffbce18bd2e32c0
bdewitt <dewitt.b@husky.neu.edu>
2019-08-03 18:41:45 -0400

    changed gpio commands

--- pumptestloop.sh
+++ pumptestloop.sh
@@ -4,8 +4,8 @@
 for i in 17 27 22 23 24 25 20 21
 do
   echo "working on pin $i"
-  gpio mode i out
-  gpio write i 0
-  sleep 3s
-  gpio write i 1
+  gpio mode $i out
+  gpio write $i 0
+  sleep 2s
+  gpio write $i 1
 done
